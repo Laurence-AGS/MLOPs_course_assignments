7aa32504a6ee79eb1d443e502ed9b924a25d9a95
Laurece-AGS <s-laurence.shehata@zewailcity.edu.eg>
2026-03-26 12:36:16 +0200

    fixing yaml file and checking file

--- check_threshold.py
+++ check_threshold.py
@@ -3,7 +3,7 @@ from mlflow.tracking import MlflowClient
 
 THRESHOLD = 0.85
 
-mlflow.set_tracking_uri("file:./mlruns")
+# mlflow.set_tracking_uri("file:./mlruns")  # Removed to respect environment variable
 
 with open("model_info.txt", "r") as f:
     run_id = f.read().strip()
